46b51a5afebb35e6fa6992aa99f07e2d73006596
tillenotuoma <totuoma27@gmail.com>
2025-01-24 13:01:10 +0300

    Worked on the BookList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
 import './App.css'
+import BookList from './components/BookList'
 
 function App() {
 
   return (
     <>
-   
+   <h1>Digital Library App</h1>
+   <BookList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/BookList.jsx
@@ -0,0 +1,52 @@
+import BookItem from './BookItem'
+import AddBookForm from './AddBookForm'
+import { useState } from 'react'
+import { useEffect } from 'react';
+
+function BookList() {
+    const [books, setBooks] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:4000/books")
+          .then((response) => response.json())
+          .then((data) => setBooks(data))
+          .catch((error) => console.error("Error fetching books:", error));
+      }, []);
+
+      const filteredBooks = books.filter((book) =>
+        book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      book.genre.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+
+      function handleAddBook(newBook) {
+        console.log("Adding book:", newBook);
+        setBooks([...books, newBook]);
+
+        fetch("http://localhost:4000/books", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newBook),
+        })
+          .then((response) => response.json())
+          .then((data) => setBooks([...books, data]))
+          .catch((error) => console.error("Error adding book:", error));
+      }
+
+  return (
+    <>
+    <input type='text' placeholder='Search by title or genre' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+    <h1>This is the Digital Library Component</h1>
+    <ul>
+      {filteredBooks.map((book, index) => (
+        <BookItem key={index} taskItem={book} />
+      ))}
+    </ul>
+    <AddBookForm onAddBook={handleAddBook} />
+    </>
+  )
+}
+
+export default BookList
